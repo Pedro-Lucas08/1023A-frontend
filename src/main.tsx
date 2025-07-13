42fcf2fc5d171b8ece87bfb1ac37deb447d699c8
PedroLucas <118374542+Pedro-Lucas08@users.noreply.github.com>
2025-07-13 16:18:36 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,18 @@ import { createRoot } from 'react-dom/client'
 //import './index.css'
 //import App from './App.tsx'
 //import Container from './Container.tsx'
+import { BrowserRouter } from 'react-router-dom';
 import Pagina from './Pagina'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <BrowserRouter>
+      {<Pagina/>}
+    </BrowserRouter>
     {/* <Container nome='Header'/>
     <Container nome='Body'/>
     <Container nome='Footer'/> */}
-    <Pagina/>
+    
   </StrictMode>,
 )
 
